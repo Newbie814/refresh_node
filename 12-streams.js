2a5a5b597d23c52e9b172a6525d7646b7eba289f
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-01 14:24:25 -0400

    added notes to streams

--- 12-streams.js
+++ 12-streams.js
@@ -1,5 +1,12 @@
 const { createReadStream } = require('fs');
 
+// default 64kb
+// last buffer - remainder
+// can include an options object as a second argument
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+
 const stream = createReadStream('./content/big.txt');
 
 stream.on('data', (result) => console.log(result));
