2d5b38e844b387c59bcb46ad81aecc5a859d9d7e
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-05-01 21:22:38 -0400

    created server stream

--- 12-streams.js
+++ 12-streams.js
@@ -10,3 +10,5 @@ const { createReadStream } = require('fs');
 const stream = createReadStream('./content/big.txt');
 
 stream.on('data', (result) => console.log(result));
+
+stream.on('error', (err) => console.log(err));
